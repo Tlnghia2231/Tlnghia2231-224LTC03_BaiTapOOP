2e0900123957ca441c0aae6ea43a1230c8acdf9b
Tlnghia2231 <22115053122231@sv.ute.udn.vn>
2025-03-06 21:54:02 +0700

    Tao lop Employee

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,6 +6,40 @@
         string GetName();
     }
 
+    public abstract class Employee : IEmployee
+    {
+        private string name;
+        private int paymentPerHour;
+
+        public Employee(string name, int paymentPerHour)
+        {
+            this.name = name;
+            this.paymentPerHour = paymentPerHour;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public int GetPaymentPerHour()
+        {
+            return paymentPerHour;
+        }
+
+        public void SetPaymentPerHour(int paymentPerHour)
+        {
+            this.paymentPerHour = paymentPerHour;
+        }
+
+        public abstract int CalculateSalary();
+    }
+
     class Program
     {
         static void Main()
